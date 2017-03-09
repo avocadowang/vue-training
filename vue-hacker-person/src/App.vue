555c38ce4a26bbe332de4b8068c3b13ede2975bb
wneil <540163205@qq.com>
2017-03-09 15:31:48 +0800

    no message

--- vue-hacker-person/src/App.vue
+++ vue-hacker-person/src/App.vue
@@ -1,7 +1,25 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <header class="header">
+      <nav class="inner">
+        <router-link to="/" exact>
+          <img class="logo" src="../public/logo-48.png" alt="logo">
+        </router-link>
+        <router-link to="/top">TOP</router-link>
+        <router-link to="/new">NEW</router-link>
+        <router-link to="/show">SHOW</router-link>
+        <router-link to="/ask">ASK</router-link>
+        <router-link to="/job">JOBS</router-link>
+        <a class="github" href="https://github.com/vuejs/vue-hackernews-2.0" target="_blank">
+          Built with Vue.js
+        </a>
+      </nav>
+    </header>
+    <transition name="fade" mode="out-in">
+      <!--router是一个功能性组件,渲染路径匹配到的视图组件,可内嵌-->
+      <!--默认name=default,若多个需要设置name为其他值-->
+      <router-view class="view"></router-view>
+    </transition>
   </div>
 </template>
 
@@ -10,14 +28,14 @@ export default {
   name: 'app'
 }
 </script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  *{
+    margin: 0;
+    border: 0;
+  }
+  header{
+    background-color: rgb(255,78,0);
+  }
 </style>
+
+
